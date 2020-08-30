b7e3736adf8588ecf675e19788c1288c528e48a6
equation314 <equation618@gmail.com>
2020-08-30 18:21:42 +0800

    Step 1: implement visitors

--- src/visitor/codegen.ts
+++ src/visitor/codegen.ts
@@ -1,10 +1,32 @@
 import { AbstractParseTreeVisitor } from "antlr4ts/tree";
 import { MiniDecafVisitor } from "../gen/MiniDecafVisitor";
+import MiniDecafParser = require("../gen/MiniDecafParser");
 
 /** 语法树到 RV32 汇编码的生成器 */
 export class Riscv32CodeGen extends AbstractParseTreeVisitor<string>
     implements MiniDecafVisitor<string> {
     defaultResult(): string {
-        return "nop";
+        return "";
+    }
+
+    visitProgram(ctx: MiniDecafParser.ProgramContext): string {
+        return ctx.func().accept(this);
+    }
+
+    visitFunc(ctx: MiniDecafParser.FuncContext): string {
+        let asm = ".globl main\nmain:\n";
+        asm += ctx.stmt().accept(this);
+        return asm;
+    }
+
+    visitStmt(ctx: MiniDecafParser.StmtContext): string {
+        let asm = ctx.expr().accept(this);
+        asm += "    ret\n";
+        return asm;
+    }
+
+    visitExpr(ctx: MiniDecafParser.ExprContext): string {
+        let int = parseInt(ctx.Integer().text);
+        return `    li a0, ${int}\n`;
     }
 }
--- src/visitor/executor.ts
+++ src/visitor/executor.ts
@@ -1,9 +1,27 @@
 import { AbstractParseTreeVisitor } from "antlr4ts/tree";
 import { MiniDecafVisitor } from "../gen/MiniDecafVisitor";
+import MiniDecafParser = require("../gen/MiniDecafParser");
 
 /** 解释执行器，直接在语法树上执行 */
 export class Executor extends AbstractParseTreeVisitor<number> implements MiniDecafVisitor<number> {
     defaultResult(): number {
         return 0;
+    }
+
+    visitProgram(ctx: MiniDecafParser.ProgramContext): number {
+        return ctx.func().accept(this);
+    }
+
+    visitFunc(ctx: MiniDecafParser.FuncContext): number {
+        return ctx.stmt().accept(this);
+    }
+
+    visitStmt(ctx: MiniDecafParser.StmtContext): number {
+        return ctx.expr().accept(this);
+    }
+
+    visitExpr(ctx: MiniDecafParser.ExprContext): number {
+        let int = parseInt(ctx.Integer().text);
+        return int;
     }
 }
